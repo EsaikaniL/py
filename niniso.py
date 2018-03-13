aa286c69aab4f7213aba49e1b8ec9212962cb08c
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-13 18:02:02 +0530

    tc iso

---
+++ niniso.py
@@ -0,0 +1,7 @@
+n=int(input("enter first num:"))
+m=int(input("enter second num:"))
+if (n == m):
+    print("not a isogram isogram")
+else:
+    print("is isogram")
+#even if one number is repeted it an isogram
