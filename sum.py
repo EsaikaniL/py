4f23f6b4a349c2bfa703b8645fd123e7e6b14277
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-15 19:31:28 +0530

    tf +

---
+++ sum.py
@@ -0,0 +1,4 @@
+n=int(input("enter the no. of element needed :"))
+x=[int(x) for x in input("enter the elements by giving whitespaces:").split()]
+print(n,("is the no. of elements in array."))
+print(sum(x),("is the total of the elements in the list."))
