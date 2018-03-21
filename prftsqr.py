973154c5351aae77a186947620f030387e81d8b7
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-21 20:35:24 +0530

    Delete prftsqr.py

--- prftsqr.py
+++
@@ -1,6 +0,0 @@
-m=int(input("enter the range of list:"))
-o=int(input("enter the range upto:"))
-n = [x for x in range(m,o) if int(x**0.5) == x**0.5]
-print("interval is btwn",m , "and" ,o )
-print(n,"is/are the perfect square of the given interval" )
-print(int(len(n)),("is the no. of perfect squares in list"))
