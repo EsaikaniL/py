bed60740e65514efdd08e3023087bebe6e1f4a00
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-22 16:17:32 +0530

    repetation

---
+++ repeat.py
@@ -0,0 +1,6 @@
+a=input("enter the string/sentance:")
+b=input("enter the char :")
+if (a.count(b)==0):
+    print("there is no repetition.")
+else:
+    print(a.count(b),("times"),b,("is repeated"))
