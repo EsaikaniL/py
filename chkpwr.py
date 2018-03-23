c7f26a42ed98a29295d33983296684ee8192d166
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-23 11:10:15 +0530

    Create chkpwr.py

---
+++ chkpwr.py
@@ -0,0 +1,6 @@
+num=int(input("Enter the number:"))
+a=int(input("Enter the number to check whether it is a power of that :"))
+if(num**0.5== int(a)):
+	print(num,("is a power of"),a)
+else:
+	print("it does not belongs to the power of the given num")
