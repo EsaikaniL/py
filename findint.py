d899331528618fca01c4c56c1b0e19f9f2327036
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-23 12:21:43 +0530

    tf int

--- findint.py
+++ findint.py
@@ -1,5 +1,5 @@
-num=(input("enter something:"))
-if (num)==int:
+num=int(input("enter something:"))
+if type(num)==int:
     print("yes, the given num is an integer.")
 else:
 	print("the enetred value is not an integer.")
