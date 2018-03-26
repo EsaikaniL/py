6a92ea7b072e96d06f459b393fb80933ba87317d
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-26 19:18:24 +0530

    in sens str comp

---
+++ sameinsen.py
@@ -0,0 +1,6 @@
+a=input("enter first word:")
+b=input("enter second sentance:")
+if a.lower() == b.lower() or a.upper()== b.upper():
+    print("yes, both the words are equal")
+else:
+    print("no, the words are not equal")
