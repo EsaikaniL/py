b4011b95debe334d5fcd35c5af42f3e10506dfb8
Swaathe Ganesan <36266081+swaathe@users.noreply.github.com>
2018-03-27 10:07:43 +0530

    tc common

---
+++ commonwrd.py
@@ -0,0 +1,6 @@
+a=input("enter the sentence/word:")
+b=input("enter the word:")
+if b in a :
+    print("yes, the entered word is common in the sentence")
+else:
+     print("no , there are no words in common")
